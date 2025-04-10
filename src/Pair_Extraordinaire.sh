4d39dbe1cdac648d81f4dbdc32d7a275828198ee
TomBos <tombos255@gmail.com>
2025-04-10 15:25:38 +0200

    Better variable names

--- src/Pair_Extraordinaire.sh
+++ src/Pair_Extraordinaire.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Source distro file (branch names i guess)
-mapfile -t distros < distro_word_list.txt
+# Source wordlist (branch names)
+mapfile -t words < word_list.txt
 
-for distro in "${distros[@]}"; do
-    echo "Processing $distro"
-    
+for word in "${words[@]}"; do
     # Create new branch
-    git checkout -b "$distro"
+    git checkout -b "$word"
 
     # Add commit
     git commit --allow-empty -m "Totally Legit Co-Author
@@ -15,12 +13,12 @@ for distro in "${distros[@]}"; do
     Co-authored-by: Arch Warden <tombos255+archwarden@gmail.com>"
 
     # Push commit to current branch
-    git push --set-upstream origin "$distro"
+    git push --set-upstream origin "$word"
 
     # Create PR from $distro to master
-    gh pr create --base master --head "$distro" --title "PR from $distro to master" --body "Pull request from $distro to master."
+    gh pr create --base master --head "$word" --title "PR from $word to master" --body "Pull request from $word to master."
 
     # Merge the PR and delete $distro branch 
-    gh pr merge "$distro" --merge --delete-branch
+    gh pr merge "$word" --merge --delete-branch
 done
 
