7f94358158bfe89109b5aeacbb32bad825203d6f
TomBos <tombos255@gmail.com>
2025-04-10 13:41:25 +0200

    Removed subproccess to improve performance

--- Pair_Extraordinaire.sh
+++ Pair_Extraordinaire.sh
@@ -5,6 +5,22 @@ mapfile -t distros < distro_word_list.txt
 
 for distro in "${distros[@]}"; do
     echo "Processing $distro"
-    ./coauthor.sh "$distro" 
+    
+    # Create new branch
+    git checkout -b "$distro"
+
+    # Add commit
+    git commit --allow-empty -m "Totally Legit Co-Author
+
+    Co-authored-by: Arch Warden <tombos255+archwarden@gmail.com>"
+
+    # Push commit to current branch
+    git push --set-upstream origin "$distro"
+
+    # Create PR from $distro to master
+    gh pr create --base master --head "$distro" --title "PR from $distro to master" --body "Pull request from $distro to master."
+
+    # Merge the PR and delete $distro branch 
+    gh pr merge "$distro" --merge --delete-branch
 done
 
