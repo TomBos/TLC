c9d8f57bc3c64c26989588d17a9af9b625e3c9bd
TomBos <tombos255@gmail.com>
2025-04-10 23:46:18 +0200

    Compilation safechek

--- src/00_compile.sh
+++ src/00_compile.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
+GIT_ROOT=$(git rev-parse --show-toplevel)
+OUTPUT_FILE="$GIT_ROOT/TLC.sh"
 
-echo "#!/usr/bin/env bash" >> "TLC.sh"
+echo "#!/usr/bin/env bash" >> "$OUTPUT_FILE"
 
 # Add content of Pull Shark File and add indentation
 echo "pull_shark () {
 $(cat Pull_Shark.sh | sed 's/^/  /')
-}" >> "TLC.sh"
+}" >> "$OUTPUT_FILE"
 
 # Add content of Pair Extraordinaire and indentation
 echo "pair_extraordinaire () {
 $(cat Pair_Extraordinaire.sh | sed 's/^/  /')
-}" >> "TLC.sh"
+}" >> "$OUTPUT_FILE"
 
-cat "main.sh" >> "TLC.sh"
+cat "main.sh" >> "$OUTPUT_FILE"
 
 echo "Ok!"
 
