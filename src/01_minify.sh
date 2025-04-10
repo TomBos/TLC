269fd4367b2e3fd38157f4e94d7f9091d6e4ca27
TomBos <tombos255@gmail.com>
2025-04-10 16:05:10 +0200

    Fixed dir not being created

--- src/01_minify.sh
+++ src/01_minify.sh
@@ -3,6 +3,10 @@
 # Dont include this files
 EXCLUDED=("01_minify.sh" "00_compile.sh")
 
+# Ensure dir exists
+mkdir -p ./minified
+
+# Loop trought all .sh file except the excluded ones
 for f in ./*.sh; do
   if [[ " ${EXCLUDED[*]} " =~ " $(basename "$f") " ]]; then
     echo "Excluding $f"
