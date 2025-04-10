581839b98c7ac48a810f16625e656ea339137a37
TomBos <tombos255@gmail.com>
2025-04-10 11:04:33 +0200

    Totally Legit Co-Author
    Co-authored-by: Arch Warden <tombos255+archwarden@gmail.com>

--- coauthor.sh
+++ coauthor.sh
@@ -1,4 +1,29 @@
-git commit -m "Totally Legit Co-Author
+#!/usr/bin/env bash
+
+
+if [ -z "$1" ]; then
+  echo "Branch name not set!"
+  echo "Exiting..."
+  exit 1
+fi
+
+
+# Create new branch
+git checkout -b "$1"
+
+
+# Push branch to remote
+git push --set-upstream origin "$1"
+
+
+# Add commit
+git commit --allow-empty -m "Totally Legit Co-Author
 
 Co-authored-by: Arch Warden <tombos255+archwarden@gmail.com>"
 
+
+# Create PR from $1 to master
+gh pr create --base master --head "$1" --title "PR from $1 to master" --body "Pull request from $1 to master."
+
+# Merge the PR and delete $1 branch 
+gh pr merge "$1" --merge --delete-branch
