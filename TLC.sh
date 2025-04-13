754cb2061766d3de65bc923f967e3426e9d538d6
Arch Warden <tombos255+archwarden@gmail.com>
2025-04-13 12:06:02 +0000

    Deploy to GitHub Pages

--- TLC.sh
+++ TLC.sh
@@ -12,7 +12,7 @@ create_pull_request() {
     gh pr create --base master --head "$word" --title "PR from $word to master" --body "Pull request from $word to master."
     gh pr merge "$word" --merge --delete-branch
   done
-}
+};
 display_banner() {
  cat << "EOF" 
 __/\\\\\\\\\\\\\\\__/\\\____________________/\\\\\\\\\_        
@@ -25,6 +25,66 @@ _____________\/\\\_______\/\\\______________\///\\\__________
 ______________\/\\\_______\/\\\\\\\\\\\\\\\____\////\\\\\\\\\_ 
 _______________\///________\///////////////________\/////////__
 EOF
+};
+make_selection() {
+  echo -e "Select which GitHub achievement you want to unlock:
+1) Pair Extraordinaire     2) Pull Shark
+3) YOLO                    4) Quickdraw \n"
+  read -p "Selected: " SELECTION
+  case $SELECTION in 
+    1)
+      echo "Selected: 1) Pair Extraordinaire"
+      ;;
+    2)
+      echo "Selected: 2) Pull Shark"
+      ;;
+    3)
+      echo "Selected: 3) YOLO"
+      ;;
+    4)
+      echo "Selected: 4) Quickdraw"
+      echo "❌This option is not implemented yet, please come back later"
+      make_selection
+      ;;  
+    *)
+      echo "❌Unknown Option !"
+      make_selection
+      ;;
+  esac
+};
+choose_number_of_prs() {
+  case $1 in 
+    1)
+      echo "Selected: 1) Steady Stream"
+      echo "Code will create 50 PRs to achieve this badge"
+      BRANCH_COUNT=50
+      ;;
+    2)
+      echo "Selected: 2) Custom PR Count"
+      read -rp "Enter the number of PRs you want (1–150): " input
+      if [[ "$input" =~ ^[0-9]+$ ]] && (( input >= 1 && input <= 150 )); then
+        BRANCH_COUNT=$input
+        echo "Code will create $BRANCH_COUNT PRs"
+      else
+        echo "❌ Invalid input. Please enter a number between 1 and 150."
+        choose_number_of_prs 2
+      fi
+      ;;
+    3)
+      echo "Selected: 3) One Shot"
+      echo "Code will create 1 PR to achieve this badge"
+      BRANCH_COUNT=1
+      ;;
+    4)
+      echo "Selected: 4) Quickdraw"
+      echo "❌ This option is not implemented yet. Please come back later."
+      make_selection
+      ;;  
+    *)
+      echo "❌ Unknown option!"
+      make_selection
+      ;;
+  esac 
 }
 echo "Checking Github CLI status"
 gh auth status &>/dev/null
@@ -33,8 +93,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 clear && display_banner
-echo -e "Select which GitHub achievement you want to unlock:
-1) Pair Extraordinaire     2) Pull Shark
-3) YOLO                    4) Quickdraw \n"
-read -p "Selected: " SELECTION
-echo "You selected: $SELECTION"
+make_selection
+echo "$SELECTION"
+choose_number_of_prs "$SELECTION"
