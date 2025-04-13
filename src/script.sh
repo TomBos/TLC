35577c79cae430c3d1944aebd42a28233b9278cd
TomBos <tombos255@gmail.com>
2025-04-13 14:05:48 +0200

    Script update

--- src/script.sh
+++ src/script.sh
@@ -23,7 +23,7 @@ create_pull_request() {
   done
 
 
-}
+};
 
 display_banner() {
  cat << "EOF" 
@@ -39,8 +39,81 @@ _______________\///________\///////////////________\/////////__
 
 EOF
 
-}
+};
 
+make_selection() {
+  echo -e "Select which GitHub achievement you want to unlock:
+1) Pair Extraordinaire     2) Pull Shark
+3) YOLO                    4) Quickdraw \n"
+
+  read -p "Selected: " SELECTION
+
+  case $SELECTION in 
+
+    1)
+      echo "Selected: 1) Pair Extraordinaire"
+      ;;
+    
+    2)
+      echo "Selected: 2) Pull Shark"
+      ;;
+
+    3)
+      echo "Selected: 3) YOLO"
+      ;;
+
+    4)
+      echo "Selected: 4) Quickdraw"
+      echo "❌This option is not implemented yet, please come back later"
+      make_selection
+      ;;  
+      
+    *)
+      echo "❌Unknown Option !"
+      make_selection
+      ;;
+  esac
+
+};
+
+choose_number_of_prs() {
+  case $1 in 
+    1)
+      echo "Selected: 1) Steady Stream"
+      echo "Code will create 50 PRs to achieve this badge"
+      BRANCH_COUNT=50
+      ;;
+    
+    2)
+      echo "Selected: 2) Custom PR Count"
+      read -rp "Enter the number of PRs you want (1–150): " input
+      if [[ "$input" =~ ^[0-9]+$ ]] && (( input >= 1 && input <= 150 )); then
+        BRANCH_COUNT=$input
+        echo "Code will create $BRANCH_COUNT PRs"
+      else
+        echo "❌ Invalid input. Please enter a number between 1 and 150."
+        choose_number_of_prs 2
+      fi
+      ;;
+    
+    3)
+      echo "Selected: 3) One Shot"
+      echo "Code will create 1 PR to achieve this badge"
+      BRANCH_COUNT=1
+      ;;
+
+    4)
+      echo "Selected: 4) Quickdraw"
+      echo "❌ This option is not implemented yet. Please come back later."
+      make_selection
+      ;;  
+      
+    *)
+      echo "❌ Unknown option!"
+      make_selection
+      ;;
+  esac 
+}
 
 echo "Checking Github CLI status"
 
@@ -52,14 +125,8 @@ if [ $? -ne 0 ]; then
 fi
 
 clear && display_banner
-
-
-echo -e "Select which GitHub achievement you want to unlock:
-1) Pair Extraordinaire     2) Pull Shark
-3) YOLO                    4) Quickdraw \n"
-
-
-read -p "Selected: " SELECTION
-echo "You selected: $SELECTION"
+make_selection
+echo "$SELECTION"
+choose_number_of_prs "$SELECTION"
 
 
